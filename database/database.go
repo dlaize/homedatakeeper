db94b4120e9c737eae0721ad39d36d967d1805a7
David <david.laize@gmail.com>
2017-06-09 20:43:41 +0200

    add host env var for postgres

--- database/database.go
+++ database/database.go
@@ -13,12 +13,14 @@ var (
 	DB *sql.DB
 )
 
+//Connect to PostGres DB
 func Connect(port string) {
+	host := os.Getenv("APP_HOST")
 	user := os.Getenv("APP_DB_USERNAME")
 	password := os.Getenv("APP_DB_PASSWORD")
 	dbname := os.Getenv("APP_DB_NAME")
 	connectionString :=
-		fmt.Sprintf("host=localhost port=%s user=%s password=%s dbname=%s sslmode=disable", port, user, password, dbname)
+		fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
 	var err error = nil
 	DB, err = sql.Open("postgres", connectionString)
